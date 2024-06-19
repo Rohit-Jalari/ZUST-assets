fa4d44c0f9e0057e0440400a666704b9e5004b38
Rohit Jalari <Rohitjalari3@gmail.com>
2024-06-19 15:22:14 -0700

    Customization

---
+++ includes/linkHeads.php
@@ -0,0 +1,16 @@
+<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/animate.min.css">
+<link rel="stylesheet" href="assets/css/remixicon.css">
+<link rel="stylesheet" href="assets/css/flaticon.css">
+<link rel="stylesheet" href="assets/css/jquery-ui.min.css">
+<link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+<link rel="stylesheet" href="assets/css/simplebar.min.css">
+<link rel="stylesheet" href="assets/css/metismenu.min.css">
+<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/css/dark.css">
+<link rel="stylesheet" href="assets/css/responsive.css">
+
+
+<link rel="icon" type="image/png" href="assets/images/favicon.png">
